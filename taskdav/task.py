c652c4917febbb1bc45e009dc1dce52e0faef029
David Fraser <davidf@sjsoft.com>
2013-04-15 05:29:42 +0200

    Start forming a class

--- taskdav/task.py
+++ taskdav/task.py
@@ -8,26 +8,33 @@ import caldav
 import urllib2
 import short_id
 
-url = cfg.get('server', 'url').replace("://", "://%s:%s@" % (cfg.get('server', 'username'), cfg.get('server', 'password'))) + "dav/%s/" % (cfg.get('server', 'username'),)
-
-client = caldav.DAVClient(url)
-principal = caldav.Principal(client, url)
-calendars = principal.calendars()
-
-calendar_lookup = {}
-
 def get_object_urlname(davobject):
     """Returns the last component of the url path as the object's name"""
     name = davobject.url.path.rstrip("/")
     name = name if "/" not in name else name[name.rfind("/")+1:]
     return urllib2.unquote(name)
 
-for calendar in calendars:
-    name = calendar.name or get_object_urlname(calendar)
-    # print (calendar.url.geturl(), name, calendar.id)
-    calendar_lookup[name] = calendar
+class TaskDAVClient(caldav.DAVClient):
+    """Client that knows about tasks"""
+    def __init__(self, url):
+        caldav.DAVClient.__init__(self, url)
+        # cache a principal we can use
+        self.principal = caldav.Principal(self, url)
+        self.cache_calendars()
 
-tasks_calendar = calendar_lookup["Tasks"]
+    def cache_calendars(self):
+        self.calendar_lookup = {}
+        calendars = self.principal.calendars()
+        for calendar in calendars:
+            name = calendar.name or get_object_urlname(calendar)
+            # print (calendar.url.geturl(), name, calendar.id)
+            self.calendar_lookup[name] = calendar
+
+url = cfg.get('server', 'url').replace("://", "://%s:%s@" % (cfg.get('server', 'username'), cfg.get('server', 'password'))) + "dav/%s/" % (cfg.get('server', 'username'),)
+client = TaskDAVClient(url)
+client.cache_calendars()
+
+tasks_calendar = client.calendar_lookup["Tasks"]
 
 tasks = tasks_calendar.events()
 task_lookup = short_id.prefix_dict()
